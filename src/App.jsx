8e15d5b9ed9fc78da48b8d1622d1af6cfee48004
favourdan <106870617+favourdan@users.noreply.github.com>
2023-09-25 14:25:18 +0100

    designed and consumed chat-bot feature

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,21 @@ import NotFound from "./pages/NotFound";
 import Testing from "./pages/Testing";
 import OtpInput from "./pages/Otp/OtpInput";
 import DashBoard from "./pages/DashBoard/DashBoard";
+import Layout from "./components/modules/layout/Layout";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/otpInput" element={<OtpInput />} />
-        <Route path="/dashboard" element={<DashBoard />} />
-        <Route path="/signInPage" element={<SignInPage />} />
-        <Route path="/signUpPage" element={<SignUpPage />} />
-        <Route path="/testing" element={<Testing />} />
-        <Route path="*" element={<NotFound />} />
+      <Route path='/' element={<Layout />}>
+          <Route index element={<Landing />} />
+          <Route path="/otpInput" element={<OtpInput />} />
+          <Route path="/dashboard" element={<DashBoard />} />
+          <Route path="/signInPage" element={<SignInPage />} />
+          <Route path="/signUpPage" element={<SignUpPage />} />
+          <Route path="/testing" element={<Testing />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </>
   );
